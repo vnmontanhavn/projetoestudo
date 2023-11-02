0e783a663280ce66aeff374bb93a87ce59634c4f
vnmontanhavn <vinicius.newescravo@gmail.com>
2023-11-02 17:32:20 -0300

    adiciona endipoints de conta

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ const gerartoken = require("../utils/gerartoken")
 const verificartoken = require("../middleware/verificartoken")
 const router = express.Router();
 const Cliente = require("../models/cliente")
+const Conta = require("../models/conta")
 const config= require("../config/settings")
 const jwt = require("jsonwebtoken")
 
@@ -20,7 +21,7 @@ router.get("/",(req,res)=>{
     })
 })
 
-
+// O Cadastro de usuarios não exige token, pq quem está cadastrando usuario ainda não tem login.
 router.post("/insert",(req,res)=>{
 
     bcrypt.hash(req.body.senha,config.bcrypt_salt,(err,cripto)=>{
@@ -37,7 +38,7 @@ router.post("/insert",(req,res)=>{
   })
 })
 
-
+// Login de usuario
 router.post("/login",(req,res)=>{
     const user = req.body.usuario
     const password = req.body.senha
@@ -58,7 +59,13 @@ router.post("/login",(req,res)=>{
     }).catch((err)=>res.status(500).send({output:`Erro ao processar o login -> ${err}`}))
 })
 
-
+/*
+Atualiza senha
+Aqui primeiro se verifica o token, 
+depois faz um fluxo proximo do login, 
+pra garantir q o usuario e senha correspondem, 
+por fim faz a alteração de senha
+*/
 router.post("/newpassword",(req,res)=>{
     const user = req.body.usuario
     const oldPassword = req.body.senha_antiga
@@ -68,6 +75,7 @@ router.post("/newpassword",(req,res)=>{
     const token = req.headers['authorization'];
     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
     jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
         if(newPassword != repetNew){
             return res.status(400).send({output:`Nova senha e sua repetição não correspondem`})
         }
@@ -92,11 +100,12 @@ router.post("/newpassword",(req,res)=>{
     })
 })
 
-
+//update exige token por segurança
 router.put("/update/:id",(req, res)=>{
     const token = req.headers['authorization'];
     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
     jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
         Cliente.findByIdAndUpdate(req.params.id,req.body,{new:true}).then((result)=>{
             if(!result){
                 res.status(400).send({output:`Não foi possível localizar`})
@@ -106,12 +115,70 @@ router.put("/update/:id",(req, res)=>{
     })
 })
 
+//delete exige token
 router.delete("/delete/:id",(req,res)=>{
-    Cliente.findByIdAndDelete(req.params.id).then((result)=>{
-        res.status(204).send({output:`Apagado`})
-    }).catch((error)=>res.status(500).send({output:`Erro ao tentar apagar`,erro:error}))
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+        Cliente.findByIdAndDelete(req.params.id).then((result)=>{
+            res.status(204).send({output:`Apagado`})
+        }).catch((error)=>res.status(500).send({output:`Erro ao tentar apagar`,erro:error}))
+    })
 })
 
+//EndPoints da Conta
+//Todos so endpoints de conta exigem token, pois são ações internas.
+router.get("/contas",(req,res)=>{
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+
+        Conta.find().then((result)=>{
+            res.status(200).send({output:`Ok`,payload:result})
+        }).catch((error)=>res.status(500).send({output:`Erro ao processar o pedido`,err:error}))
+    })
+})
+
+router.post("/contas/insert",(req,res)=>{
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+        const dados = new Conta(req.body);
+        dados.save().then((result)=>{
+            res.status(201).send({output:`Cadastrado`,payload:result})
+        }).catch((error)=>res.status(400).send({output:`Não foi possível cadastrar`,err:error}))
+    })
+})
+
+router.put("/contas/update/:id",(req, res)=>{
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+        Conta.findByIdAndUpdate(req.params.id,req.body,{new:true}).then((result)=>{
+            if(!result){
+                res.status(400).send({output:`Não foi possível localizar`})
+            }
+            res.status(200).send({ouptut:`Atualizado`,payload:result})
+        }).catch((error)=>res.status(500).send({output:`Erro ao tentar atualizar`,erro:error}))
+    })
+})
+
+router.delete("/contas/delete/:id",(req,res)=>{
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    jwt.verify(token, process.env.JWT_KEY, function(err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+        Conta.findByIdAndDelete(req.params.id).then((result)=>{
+            res.status(204).send({output:`Apagado`})
+        }).catch((error)=>res.status(500).send({output:`Erro ao tentar apagar`,erro:error}))
+    })
+})
+
+// Endpoint de excessão
 router.use((req,res)=>{
     res.type("application/json");
     res.status(404).send({mensagem:"404 - Not Found"})
